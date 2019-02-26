cb04224d326eb4b502e8efece02700768377b4e3
Nida Hameed <nidahameed@gmail.com>
2019-02-25 18:25:52 -0600

    GUI commit

---
+++ app/src/main/java/com/example/twitter2gui/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.twitter2gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
