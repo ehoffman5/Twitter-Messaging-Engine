af1ddf90e3e170281cb2934cc581a1d1b75d3254
Nida Hameed <nidahameed@gmail.com>
2019-02-25 21:49:26 -0600

    Updated

--- app/src/main/java/com/example/twitter2gui/RegisterActivity.java
+++ app/src/main/java/com/example/twitter2gui/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.twitter2gui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,5 +20,19 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mTextUsername = (EditText) findViewById(R.id.edittext_username);
+        mTextPassword = (EditText) findViewById(R.id.edittext_password);
+        mTextCnfPassword = (EditText) findViewById(R.id.edittext_cnf_password);
+        mButtonRegister = (Button) findViewById(R.id.button_register);
+        mTextViewLogin = (TextView) findViewById(R.id.textview_login);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
     }
 }
